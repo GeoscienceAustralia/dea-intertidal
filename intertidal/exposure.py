38b180c969f024c9bc2b25229884473d856ee1fb
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-04-13 14:41:50 +1000

    Close #11, #12 by moving raster export and dtype change to end of workflow (#13)
    * Update code to work for date strings, not just years
    
    * Close #11, #12 by moving raster export and dtype change to end of workflow, and renaming offsets

--- intertidal/exposure.py
+++ intertidal/exposure.py
@@ -4,56 +4,67 @@ import numpy as np
 from dea_tools.coastal import pixel_tides
 
 
-def pixel_exp(
+def exposure(
     dem,
     timerange,
 ):
     """
-    Calculate exposure percentage for each pixel based on tide-height differences between the
-    NIDEM value and percentile values of the tide model for a given time range.
+    Calculate exposure percentage for each pixel based on tide-height 
+    differences between the NIDEM value and percentile values of the 
+    tide model for a given time range.
 
     Parameters
     ----------
     dem : xarray.DataArray
-        xarray.DataArray containing Digital Elevation Model (DEM) data and coordinates and
-        attributes metadata.
+        xarray.DataArray containing Digital Elevation Model (DEM) data 
+        and coordinates and attributes metadata.
     timerange : tuple
-        Tuple containing start and end time of time range to be used for tide model in the
-        format of "YYYY-MM-DD".
+        Tuple containing start and end time of time range to be used for
+        tide model in the format of "YYYY-MM-DD".
 
     Returns
     -------
     exposure : xarray.DataArray
-        An xarray.DataArray containing the percentage time 'exposure' of each pixel from seawater
-        for the duration of the modelling period `timerange`. Values are int 16.
+        An xarray.DataArray containing the percentage time 'exposure' of
+        each pixel from seawater for the duration of the modelling 
+        period `timerange`.
     tide_cq : xarray.DataArray
-        An xarray.DataArray containing the quantiled high temporal resolution tide modelling for each
-        pixel. Dimesions should be 'quantile', 'x' and 'y'.
+        An xarray.DataArray containing the quantiled high temporal 
+        resolution tide modelling for each pixel. Dimesions should be 
+        'quantile', 'x' and 'y'.
 
     Notes
     -----
-    - The tide-height percentiles range from 0 to 100, divided into 101 equally spaced values.
-    - The 'diff' variable is calculated as the absolute difference between tide model percentile
-    value and the DEM value at each pixel.
-    - The 'idxmin' variable is the index of the smallest tide-height difference (i.e., maximum
-    similarity) per pixel and is equivalent to the exposure percent.
+    - The tide-height percentiles range from 0 to 100, divided into 101 
+    equally spaced values.
+    - The 'diff' variable is calculated as the absolute difference 
+    between tide model percentile value and the DEM value at each pixel.
+    - The 'idxmin' variable is the index of the smallest tide-height 
+    difference (i.e., maximum similarity) per pixel and is equivalent 
+    to the exposure percent.
     """
 
-    ## Create the tide-height percentiles from which to calculate exposure statistics
+    # Create the tide-height percentiles from which to calculate 
+    # exposure statistics
     pc_range = np.linspace(0, 1, 101)
 
-    ## Run the pixel_tides function with the calculate_quantiles option. For each pixel, an array of tideheights is returned, corresponding to the percentiles from pc_range of the timerange-tide model that each tideheight appears in the model.
+    # Run the pixel_tides function with the calculate_quantiles option. 
+    # For each pixel, an array of tideheights is returned, corresponding
+    # to the percentiles from pc_range of the timerange-tide model that 
+    # each tideheight appears in the model.
     tide_cq, _ = pixel_tides(
         dem, resample=True, calculate_quantiles=pc_range, times=timerange
     )
 
-    ## Calculate the tide-height difference between the NIDEM value and each percentile value per pixel
+    # Calculate the tide-height difference between the NIDEM value and 
+    # each percentile value per pixel
     diff = abs(tide_cq - dem)
 
-    ## Take the percentile of the smallest tide-height difference as the exposure % per pixel
+    # Take the percentile of the smallest tide-height difference as the 
+    # exposure % per pixel
     idxmin = diff.idxmin(dim="quantile")
 
-    ## Convert to percentage then int and add to master ds
-    exposure = (idxmin * 100).astype(np.int16)
+    # Convert to percentage 
+    exposure = (idxmin * 100)
 
     return exposure, tide_cq
--- intertidal/utils.py
+++ intertidal/utils.py
@@ -1,6 +1,9 @@
 import logging
 import yaml
 import fsspec
+import pandas as pd
+import numpy as np
+from pandas.tseries.offsets import MonthBegin, MonthEnd, YearBegin, YearEnd
 from pathlib import Path
 
 
@@ -31,3 +34,130 @@ def load_config(config_path: str) -> dict:
     with fsspec.open(config_path, mode="r") as f:
         config = yaml.safe_load(f)
     return config
+
+
+def round_date_strings(date, round_type="end"):
+    """
+    Round a date string up or down to the start or end of a given time
+    period.
+
+    Parameters
+    ----------
+    date : str
+        Date string of variable precision (e.g. "2020", "2020-01",
+        "2020-01-01").
+    round_type : str, optional
+        Type of rounding to perform. Valid options are "start" or "end".
+        If "start", date is rounded down to the start of the time period.
+        If "end", date is rounded up to the end of the time period.
+        Default is "end".
+
+    Returns
+    -------
+    date_rounded : str
+        The rounded date string.
+
+    Examples
+    --------
+    >>> round_date_strings('2020')
+    '2020-12-31 00:00:00'
+
+    >>> round_date_strings('2020-01', round_type='start')
+    '2020-01-01 00:00:00'
+
+    >>> round_date_strings('2020-01', round_type='end')
+    '2020-01-31 00:00:00'
+    """
+
+    # Determine precision of input date string
+    date_segments = len(date.split("-"))
+
+    # If provided date has no "-", treat it as having year precision
+    if date_segments == 1 and round_type == "start":
+        date_rounded = str(pd.to_datetime(date) + YearBegin(0))
+    elif date_segments == 1 and round_type == "end":
+        date_rounded = str(pd.to_datetime(date) + YearEnd(0))
+
+    # If provided date has one "-", treat it as having month precision
+    elif date_segments == 2 and round_type == "start":
+        date_rounded = str(pd.to_datetime(date) + MonthBegin(0))
+    elif date_segments == 2 and round_type == "end":
+        date_rounded = str(pd.to_datetime(date) + MonthEnd(0))
+
+    # If more than one "-", then return date as-is
+    elif date_segments > 2:
+        date_rounded = date
+
+    return date_rounded
+
+
+def export_intertidal_rasters(
+    ds,
+    prefix="data/interim/testing",
+    int_bands=None,
+    int_nodata=-999,
+    int_dtype=np.int16,
+    float_dtype=np.float32,
+    overwrite=True,
+):
+    """
+    Export outputs of the DEA Intertidal workflow to COG GeoTIFF files.
+
+    If a band contains "elevation" in the name it is exported as a
+    float32 data type. Otherwise, the band is exported as an integer16
+    data type, after filling NaN with the nodata value and setting the
+    nodata attribute on the layer.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        The dataset containing the bands to be exported.
+    prefix : str, optional
+        A string that will be used as a prefix for the output file
+        names (default is "testing").
+    int_bands : tuple or list, optional
+        A list of bands to export as integer datatype. If None, will use
+        the following list of bands: ("exposure", "extents", 
+        "offset_hightide", "offset_lowtide", "spread")
+    int_nodata : int, optional
+        An integer that represents nodata values for integer bands
+        (default is -999).
+    int_dtype : string or numpy data type, optional
+        The data type to use for integer layers (default is 
+        np.int16).
+    float_dtype : string or numpy data type, optional
+        The data type to use for floating point layers (default is 
+        np.float32).
+    overwrite : bool, optional
+        A boolean value that determines whether or not to overwrite
+        existing files (default is True).
+
+    Returns
+    -------
+    None
+    """
+
+    # Use default list of bands to convert to integers if none provided
+    if int_bands is None:
+        int_bands = (
+            "exposure",
+            "extents",
+            "offset_hightide",
+            "offset_lowtide",
+            "spread",
+        )
+
+    for band in ds:
+        # Export specific bands as integer16 data types by first filling
+        # NaN with nodata value before converting to int, then setting
+        # nodata attribute on layer
+        if band in int_bands:
+            band_da = ds[band].fillna(int_nodata).astype(int_dtype)
+            band_da.attrs["nodata"] = int_nodata
+
+        # Export other bands as float32 data types
+        else:
+            band_da = ds[band].astype(float_dtype)
+
+        # Export band to file
+        band_da.odc.write_cog(fname=f"{prefix}_{band}.tif", overwrite=overwrite)
