54767baaa31a1ae7af381789711f38aa86e2e4e6
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-11-20 15:40:42 +1100

    Improve data loading efficiency using `GridSpec` and `odc.algo`; add ensemble tide modelling (#30)
    * Add topobathy and multiple tide model support
    
    * Update all package versions to match current Sandbox image
    
    * Add code for clipping multiple models, rename notebook
    
    * Update notebooks to use topobathy and tide model selection
    
    * Use latest version of DEA Tools
    
    * Extrapolate all points
    
    * Add additional tide models to clipping notebook
    
    * Update elevation code to use new study area selection
    
    * Updates to validation code
    
    * Add new area to dev study area file
    
    * Relocate elevation notebook for ease of access
    
    * Relocate exposure notebook to experimental
    
    * Update workflow notebook and rerun
    
    * Black formatting
    
    * Add notebook for exporting new version of 32 km tiles based on gridspec
    
    * Add updated composite notebook
    
    * Add geojson versions of gridspec grid
    
    * Delete study area file
    
    * Refactor composite code to use gridspec, odc-algo tools
    
    * Refactor elevation code to use gridspec, odc-algo tools
    
    * Remove config load code (no longer needed with gridspec)
    
    * Rerun main notebooks after refactor
    
    * Update composites experimental notebook
    
    * Remove no longer needed config dir
    
    * Do reprojection via Dask to avoid transferring large Dask graph to cluster
    
    * Update to latest version of dea_Tools
    
    * Add missing docstring, extra customisation to load func
    
    * Update and rerun notebook
    
    * Update DEA_tools and simplify code
    
    * Add missing packages
    
    * Move dask client close to after tide modelling step
    
    * Add new notebooks, rearrange
    
    * Clean and rerun notebooks
    
    * Add first draft of ensemble code, minor changes to make running at scale easier while testing
    
    * Add latest version of DEA Tools, remove PyYAML
    
    * Add notebook for calculating correlations at points
    
    * Latest changes to tide modelling notebooks
    
    * Latest changes to elevation and CLI notebooks
    
    * Use resample code from dea_tools, add docu, tweak thresholds
    
    * Fix for bokeh, use latest DEA tools
    
    * Rerun elevation notebook
    
    * Rerun CLI notebook
    
    * Add example correlation file
    
    * Minor updates to elevation params, including func for debugging elevation

--- intertidal/composites.py
+++ intertidal/composites.py
@@ -9,86 +9,73 @@ import click
 
 import datacube
 import odc.geo.xr
-from odc.algo import xr_geomedian, xr_quantile
-from datacube.utils.geometry import Geometry
+from odc.algo import (
+    int_geomedian,
+    enum_to_bool,
+    keep_good_only,
+)
 from datacube.utils.aws import configure_s3_access
 
 from dea_tools.coastal import pixel_tides
 from dea_tools.dask import create_local_dask_cluster
 
-from intertidal.utils import (
-    load_config,
-    configure_logging,
-    round_date_strings,
-    export_intertidal_rasters,
-)
+from intertidal.utils import configure_logging
 from intertidal.elevation import load_data
 
 
 def intertidal_composites(
-    study_area,
-    start_date="2020",
-    end_date="2022",
-    resolution=10,
-    threshold_method="percentile",
+    satellite_ds,
     threshold_lowtide=0.2,
     threshold_hightide=0.8,
-    crs="EPSG:3577",
-    include_s2=True,
-    include_ls=False,
-    filter_gqa=False,
-    config_path="configs/dea_intertidal_config.yaml",
+    max_iters=10000,
+    tide_model="FES2014",
+    tide_model_dir="/var/share/tide_models",
+    study_area=None,
     log=None,
 ):
     """
-    Calculates Geometric Median composites of the coastal zone at low 
+    Calculates Geometric Median composites of the coastal zone at low
     and high tide using satellite imagery and tidal modeling.
-    
+
+    This function uses tools from `odc.algo` to keep data in its
+    original integer datatype until the last possible moment, improving
+    the efficiency of Dask processing.
+
     Based on the method described in:
-    
-    Sagar, S., Phillips, C., Bala, B., Roberts, D., & Lymburner, L. 
+
+    Sagar, S., Phillips, C., Bala, B., Roberts, D., & Lymburner, L.
     (2018). Generating Continental Scale Pixel-Based Surface Reflectance
     Composites in Coastal Regions with the Use of a Multi-Resolution
-    Tidal Model. Remote Sensing, 10, 480. 
-    https://doi.org/10.3390/rs10030480
+    Tidal Model. Remote Sensing, 10, 480. https://doi.org/10.3390/rs10030480
 
     Parameters
     ----------
-    study_area : int or str or Geometry
-        Study area polygon represented as either the ID of a tile grid
-        cell, or a Geometry object.
-    start_date : str, optional
-        Start date of data to load (inclusive), by default '2020'. Can
-        be any string supported by datacube (e.g. '2020-01-01')
-    end_date : str, optional
-        End date of data to load (inclusive), by default '2022'. Can
-        be any string supported by datacube (e.g. '2022-12-31')
-    resolution : int, optional
-        Pixel size in meters, by default 10.
-    threshold_method : str, optional
-        The method used to identify the lower and upper tide height
-        thresholds used to create low and high tide composites. Options
-        are "percent" which will use an absolute percentage of the total
-        tide range, or "percentile" which will take a percentile of all
-        tide observations.
+    satellite_ds : xarray.Dataset
+        A satellite data time series containing spectral bands.
     threshold_lowtide : float, optional
-        The percent or percentile used to identify low tide
-        observations, by default 0.2.
+        Quantile used to identify low tide observations, by default 0.2.
     threshold_hightide : float, optional
-        The percent or percentile used to identify high tide
-        observations, by default 0.8.
-    crs : str, optional
-        Coordinate reference system used to load data, by default
-        "EPSG:3577".
-    include_s2 : bool, optional
-        Whether to include Sentinel-2 data, by default True.
-    include_ls : bool, optional
-        Whether to include Landsat data, by default False.
-    filter_gqa : bool, optional
-        Whether to apply the GQA filter to the dataset, by default False.
-    config_path : str, optional
-        Path to the configuration file, by default
-        "configs/dea_intertidal_config.yaml".
+        Quantile used to identify high tide observations, by default 0.8.
+    max_iters : int, optional
+        Value to pass to the 'max_iters' param of `int_geomedian`. This
+        can be set to a low value (e.g. 10) to increase the processing
+        speed of test runs.
+    tide_model : str, optional
+        The tide model or a list of models used to model tides, as
+        supported by the `pyTMD` Python package. Options include:
+        - "FES2014" (default; pre-configured on DEA Sandbox)
+        - "TPXO9-atlas-v5"
+        - "TPXO8-atlas"
+        - "EOT20"
+        - "HAMTIDE11"
+        - "GOT4.10"
+    tide_model_dir : str, optional
+        The directory containing tide model data files. Defaults to
+        "/var/share/tide_models"; for more information about the
+        directory structure, refer to `dea_tools.coastal.model_tides`.
+    study_area : string, optional
+        An optional string giving the name of the analysis; used to
+        prefix log entries.
     log : logging.Logger, optional
         Logger object, by default None.
 
@@ -96,140 +83,76 @@ def intertidal_composites(
     -------
     ds_lowtide : xarray.Dataset
         xarray.Dataset object containing a geomedian of the observations
-        with the lowest X percent or percentile tide heights for each
-        pixel in the study area.
+        with the lowest X quantile tide heights for each pixel.
     ds_hightide : xarray.Dataset
         xarray.Dataset object containing a geomedian of the observations
-        with the highest X percent or percentile tide values for each
-        pixel in the study area.
+        with the highest X quantile tide values for each pixel.
     """
 
+    # Set up logs if no log is passed in
     if log is None:
         log = configure_logging()
 
-    # Create local dask cluster to improve data load time
-    client = create_local_dask_cluster(return_client=True)
-
-    # Connect to datacube
-    dc = datacube.Datacube(app="Intertidal_composites")
-
-    # Load analysis params from config file
-    config = load_config(config_path)
-
-    # Load study area from tile grid if passed a string
-    if isinstance(study_area, (int, str)):
-        # Load study area
-        gridcell_gdf = (
-            gpd.read_file(config["Input files"]["grid_path"])
-            .to_crs(crs)
-            .set_index("id")
-        )
-        gridcell_gdf.index = gridcell_gdf.index.astype(str)
-        gridcell_gdf = gridcell_gdf.loc[[str(study_area)]]
-
-        # Create geom as input for dc.load
-        geom = Geometry(geom=gridcell_gdf.iloc[0].geometry, crs=crs)
-        log.info(f"Study area {study_area}: Loaded study area grid")
-
-    # Otherwise, use supplied geom
+    # Use study area name for logs if it exists
+    if study_area is not None:
+        log_prefix = f"Study area {study_area}: "
     else:
-        geom = study_area
-        study_area = "testing"
-        log.info(f"Study area {study_area}: Loaded custom study area")
+        log_prefix = ""
 
-    # Load data
-    log.info(f"Study area {study_area}: Loading satellite data")
-    satellite_ds = load_data(
-        dc=dc,
-        geom=geom,
-        time_range=(start_date, end_date),
-        resolution=resolution,
-        crs=crs,
-        s2_prod="s2_nbart_norm" if include_s2 else None,
-        ls_prod="ls_nbart_norm" if include_ls else None,
-        config_path=config["Virtual product"]["virtual_product_path"],
-        filter_gqa=filter_gqa,
+    # Model tides into for spatial extent and timesteps in satellite data
+    log.info(f"Study area {study_area}: Modelling tide heights")
+    tides_highres, tides_lowres = pixel_tides(
+        satellite_ds,
+        model=tide_model,
+        directory=tide_model_dir,
+        cutoff=np.inf,
+        dask_compute=False,
     )
-    satellite_ds.persist()
 
-    # Model tides into every pixel in the three-dimensional (x by y by time) satellite dataset
-    log.info(f"Study area {study_area}: Modelling tide heights for each pixel")
-    tide_m, _ = pixel_tides(satellite_ds, resample=True)
+    # Start processing tide data, using .persist so we can re-use our results
+    tides_highres.persist()
 
-    # Add tide heights to satellite data array
-    log.info(f"Study area {study_area}: Add tide heights to satellite data array")
-    satellite_ds["tide_m"] = tide_m
+    # Calculate low and high tide height thresholds using quantile of
+    # all tide observations.
+    log.info(f"Study area {study_area}: Calculate low and high tide height thresholds")
+    threshold_ds = (
+        tides_lowres.quantile(q=[threshold_lowtide, threshold_hightide], dim="time")
+        .odc.assign_crs(satellite_ds.odc.geobox.crs)
+        .odc.reproject(satellite_ds.odc.geobox, resampling="bilinear")
+        .drop("quantile")
+    )
 
-    # Calculate a threshold for low and high tide composite using either
-    # an absolute percent of the total tide range, or a percentile of
-    # of all tide observations.
-    log.info(f"Study area {study_area}: Calculate low and high tide thresholds")
-    if threshold_method == "percent":
-        # Calculate max, min and full range of tide
-        tide_max = satellite_ds.tide_m.max(dim="time")
-        tide_min = satellite_ds.tide_m.min(dim="time")
-        tide_range = tide_max - tide_min
-
-        # Use tide range to calculate thresholds
-        min_thresh = tide_min + (tide_range * threshold_lowtide)
-        max_thresh = tide_min + (tide_range * threshold_hightide)
-
-    elif threshold_method == "percentile":
-        # Calculate min and max thresholds using percentiles of tides
-        tide_q = xr_quantile(
-            src=satellite_ds[["tide_m"]],
-            quantiles=[threshold_lowtide, threshold_hightide],
-            nodata=np.nan,
-        )
-        min_thresh = tide_q.sel(quantile=threshold_lowtide).tide_m
-        max_thresh = tide_q.sel(quantile=threshold_hightide).tide_m
-
-    # Select low and high tide observations
+    # Apply threshold to keep only pixels with tides less or greater than
+    # than tide height threshold
     log.info(f"Study area {study_area}: Masking to low and high tide observations")
-    ds_low = satellite_ds.where(satellite_ds.tide_m <= min_thresh)
-    ds_high = satellite_ds.where(satellite_ds.tide_m >= max_thresh)
+    low_mask = tides_highres <= threshold_ds.isel(quantile=0)
+    high_mask = tides_highres >= threshold_ds.isel(quantile=-1)
 
-    # Drop fully empty scenes to speed up geomedian
-    ds_low = ds_low.sel(time=ds_low.tide_m.isnull().mean(dim=["x", "y"]) < 1).drop(
-        "tide_m"
+    # Mask out pixels outside of selected tides. Drop fully empty scenes
+    # to speed up geomedian
+    ds_low = keep_good_only(x=satellite_ds, where=low_mask).sel(
+        time=low_mask.any(dim=["x", "y"])
     )
-    ds_high = ds_high.sel(time=ds_high.tide_m.isnull().mean(dim=["x", "y"]) < 1).drop(
-        "tide_m"
-    )   
+    ds_high = keep_good_only(x=satellite_ds, where=high_mask).sel(
+        time=high_mask.any(dim=["x", "y"])
+    )
 
-    # Calculate low tide geomedian and process with Dask
-    log.info(f"Study area {study_area}: Processing low tide geomedian")
-    ds_lowtide = xr_geomedian(ds=ds_low)
-    ds_lowtide.load()
-    
-    # Calculate high tide geomedian and process with Dask
-    log.info(f"Study area {study_area}: Processing high tide geomedian")
-    ds_hightide = xr_geomedian(ds=ds_high)
-    ds_hightide.load()
-
-    # Close Dask client
-    client.close()
+    # Calculate low and high tide geomedians
+    log.info(f"Study area {study_area}: Calculating geomedians")
+    num_threads = os.cpu_count() - 2
+    ds_lowtide = int_geomedian(ds=ds_low, maxiters=max_iters, num_threads=num_threads)
+    ds_hightide = int_geomedian(ds=ds_high, maxiters=max_iters, num_threads=num_threads)
 
     return ds_lowtide, ds_hightide
 
 
 @click.command()
 @click.option(
-    "--config_path",
-    type=str,
-    required=True,
-    help="Path to the YAML config file defining inputs to "
-    "use for this analysis. These are typically located in "
-    "the `dea-intertidal/configs/` directory.",
-)
-@click.option(
     "--study_area",
     type=str,
     required=True,
-    help="A string providing a unique ID of an analysis "
-    "gridcell that will be used to run the analysis. This "
-    'should match a row in the "id" column of the provided '
-    "analysis gridcell vector file.",
+    help="A string providing a GridSpec tile ID (e.g. in the form "
+    "'x143y56') to run the analysis on.",
 )
 @click.option(
     "--start_date",
@@ -257,15 +180,32 @@ def intertidal_composites(
     "--threshold_lowtide",
     type=float,
     default=0.2,
-    help="The percent or percentile used to identify low tide "
-    "observations. Defaults to 0.2.",
+    help="The quantile used to identify low tide observations. " "Defaults to 0.2.",
 )
 @click.option(
     "--threshold_hightide",
     type=float,
     default=0.8,
-    help="The percent or percentile used to identify high tide "
-    "observations. Defaults to 0.8.",
+    help="The quantile used to identify high tide observations. " "Defaults to 0.8.",
+)
+@click.option(
+    "--tide_model",
+    type=str,
+    multiple=True,
+    default=["FES2014"],
+    help="The model used for tide modelling, as supported by the "
+    "`pyTMD` Python package. Options include 'FES2014' (default), "
+    "'TPXO9-atlas-v5', 'TPXO8-atlas', 'EOT20', 'HAMTIDE11', 'GOT4.10'. "
+    "This parameter can be repeated to request multiple models, e.g.: "
+    "`--tide_model FES2014 --tide_model FES2012`.",
+)
+@click.option(
+    "--tide_model_dir",
+    type=str,
+    default="/var/share/tide_models",
+    help="The directory containing tide model data files. Defaults to "
+    "'/var/share/tide_models'; for more information about the required "
+    "directory structure, refer to `dea_tools.coastal.model_tides`.",
 )
 @click.option(
     "--aws_unsigned/--no-aws_unsigned",
@@ -274,13 +214,14 @@ def intertidal_composites(
     help="Whether to use sign AWS requests for S3 access",
 )
 def intertidal_composites_cli(
-    config_path,
     study_area,
     start_date,
     end_date,
     resolution,
     threshold_lowtide,
     threshold_hightide,
+    tide_model,
+    tide_model_dir,
     aws_unsigned,
 ):
     log = configure_logging(
@@ -289,46 +230,77 @@ def intertidal_composites_cli(
 
     # Configure S3
     configure_s3_access(cloud_defaults=True, aws_unsigned=aws_unsigned)
-    
+
     # Create output folder. If it doesn't exist, create it
-    output_dir = f"data/interim/{study_area}"
+    output_dir = f"data/interim/{study_area}/{start_date}-{end_date}"
     os.makedirs(output_dir, exist_ok=True)
 
     try:
-        # Calculate high and low tide geomedian composites
-        ds_lowtide, ds_hightide = intertidal_composites(
-            study_area,
-            start_date=start_date,
-            end_date=end_date,
+        log.info(f"Study area {study_area}: Loading satellite data")
+
+        # Connect to datacube to access data
+        dc = datacube.Datacube(app="Intertidal_composites_CLI")
+
+        # Lazily load Sentinel-2 satellite data
+        satellite_ds = load_data(
+            dc=dc,
+            study_area=study_area,
+            time_range=(start_date, end_date),
             resolution=resolution,
-            threshold_method="percentile",
-            threshold_lowtide=threshold_lowtide,
-            threshold_hightide=threshold_hightide,
             crs="EPSG:3577",
             include_s2=True,
             include_ls=False,
             filter_gqa=False,
-            config_path=config_path,
+            ndwi=False,
+            dtype="int16",
+        )
+
+        # Create local dask cluster to improve data load time
+        client = create_local_dask_cluster(return_client=True)
+
+        # Calculate high and low tide geomedian composites
+        log.info(f"Study area {study_area}: Running geomedians")
+        ds_lowtide, ds_hightide = intertidal_composites(
+            satellite_ds=satellite_ds,
+            threshold_lowtide=threshold_lowtide,
+            threshold_hightide=threshold_hightide,
+            max_iters=10,
+            study_area=study_area,
             log=log,
         )
+
+        # Process and load low and high tide composites using Dask
+        log.info(f"Study area {study_area}: Processing low tide composite")
+        ds_lowtide.load()
+        log.info(f"Study area {study_area}: Processing high tide composite")
+        ds_hightide.load()
+
+        # Close dask client
+        client.close()
 
         # Export layers as GeoTIFFs
         log.info(f"Study area {study_area}: Exporting outputs GeoTIFFs to {output_dir}")
 
         prefix = f"{output_dir}/{study_area}_{start_date}_{end_date}"
         ds_lowtide.to_array().odc.write_cog(
-            f"{prefix}_composite_lowtide_{int(threshold_lowtide * 100)}.tif", overwrite=True
+            f"{prefix}_composite_lowtide_{int(threshold_lowtide * 100)}.tif",
+            overwrite=True,
         )
         ds_hightide.to_array().odc.write_cog(
-            f"{prefix}_composite_hightide_{int(threshold_hightide * 100)}.tif", overwrite=True
+            f"{prefix}_composite_hightide_{int(threshold_hightide * 100)}.tif",
+            overwrite=True,
         )
 
         # Export as images
         prefix = f"data/figures/{study_area}_{start_date}_{end_date}"
-        ds_lowtide.odc.to_rgba(vmin=0.0, vmax=0.3).plot.imshow().figure.savefig(
+        ds_lowtide.odc.to_rgba(
+            bands=["nbart_red", "nbart_green", "nbart_blue"], vmin=100, vmax=2500
+        ).plot.imshow().figure.savefig(
             f"{prefix}_composite_lowtide_{int(threshold_lowtide * 100)}_rgb.png"
         )
-        ds_hightide.odc.to_rgba(vmin=0.0, vmax=0.3).plot.imshow().figure.savefig(
+        ds_hightide.odc.to_rgba(
+            bands=["nbart_red", "nbart_green", "nbart_blue"], vmin=100, vmax=2500
+        ).plot.imshow().figure.savefig(
             f"{prefix}_composite_hightide_{int(threshold_hightide * 100)}_rgb.png"
         )
 
--- intertidal/validation.py
+++ intertidal/validation.py
@@ -53,7 +53,7 @@ def eval_metrics(x, y, round=3, all_regress=False):
         "Correlation": xy_df.corr().iloc[0, 1],
         "RMSE": sqrt(mean_squared_error(xy_df.x, xy_df.y)),
         "MAE": mean_absolute_error(xy_df.x, xy_df.y),
-        "R-squared": r2_score(xy_df.x, xy_df.y),
+        "R-squared": lin_reg.rvalue ** 2,
         "Bias": (xy_df.y - xy_df.x).mean(),
         "Regression slope": lin_reg.slope,
     }
@@ -75,6 +75,7 @@ def eval_metrics(x, y, round=3, all_regress=False):
 def map_raster(
     ds,
     dst_nodata=np.nan,
+    cmap="viridis",
     vmin=None,
     vmax=None,
     display_map=True,
@@ -86,9 +87,16 @@ def map_raster(
     import folium
     import odc.geo.xr
 
-    # Turn item into a list if it isn't already
-    if not isinstance(ds, list):
-        ds = [ds]
+    # Turn dataset and visualisation params into list if not already
+    ds = [ds] if not isinstance(ds, list) else ds
+    cmap = [cmap] if not isinstance(cmap, list) else cmap
+    vmin = [vmin] if not isinstance(vmin, list) else vmin
+    vmax = [vmax] if not isinstance(vmax, list) else vmax
+    
+    # Multiply out visualisation params to length of ds
+    cmap = cmap * len(ds) if len(cmap) == 1 else cmap
+    vmin = vmin * len(ds) if len(vmin) == 1 else vmin
+    vmax = vmax * len(ds) if len(vmax) == 1 else vmax
 
     # Create folium map
     m = folium.Map(control=True)
@@ -106,7 +114,7 @@ def map_raster(
     for i, ds_i in enumerate(ds):
         # Reproject data to EPSG:3857 and add to map
         layer = ds_i.odc.reproject("epsg:3857", dst_nodata=dst_nodata).odc.add_to(
-            m, opacity=1.0, vmin=vmin, vmax=vmax
+            m, cmap=cmap[i], opacity=1.0, vmin=vmin[i], vmax=vmax[i]
         )
 
         # Use name from dataset if available, otherwise "layer 1")
@@ -131,7 +139,7 @@ def map_raster(
         display(m)
 
 
-def preprocess_validation(modelled_ds, validation_ds, clean_validation=None):
+def preprocess_validation(modelled_ds, validation_ds, clean_modelled=None, clean_validation=None):
 
     # Reproject to match array
     validation_ds = validation_ds.odc.reproject(
@@ -142,9 +150,13 @@ def preprocess_validation(modelled_ds, validation_ds, clean_validation=None):
     modelled_nodata = modelled_ds.isnull()
     validation_nodata = validation_ds.isnull()
     
+    # Optionally clean modelled dataset using mask_filters (e.g. `[('dilation', 1)]`)
+    if clean_modelled is not None:
+        modelled_nodata = mask_cleanup(modelled_nodata, mask_filters=clean_modelled)
+    
     # Optionally clean validation dataset using mask_filters (e.g. `[('dilation', 1)]`)
     if clean_validation is not None:
-        modelled_nodata = mask_cleanup(modelled_nodata, mask_filters=clean_validation)
+        validation_nodata = mask_cleanup(validation_nodata, mask_filters=clean_validation)
         
     # Export 1D modelled and validation data for valid data area
     invalid_data = modelled_nodata | validation_nodata
