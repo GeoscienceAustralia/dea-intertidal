02ecd270d89f54778f9c6a9e0756290ebd2f42bd
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-12-19 16:59:37 +1100

    Add pytest to requirements

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ from setuptools import find_packages, setup
 # Where are we?
 IS_SANDBOX = "sandbox" in os.getenv("JUPYTER_IMAGE", default="")
 
-tests_require = [
-    "pytest",
-    "pytest-dependency",
-    "pytest-cov",
-]
-
-extras = {
-    "test": tests_require,
-}
-
 # What packages are required for this module to be executed?
 REQUIRED = [
     "aiohttp",
@@ -35,6 +25,7 @@ REQUIRED = [
     "pygeos",
     "pyproj",
     "pyTMD>=2.0.0",
+    "pytest",
     "pytz",
     "rasterio",
     "setuptools-scm",
@@ -68,8 +59,6 @@ setup_kwargs = {
     "python_requires": REQUIRES_PYTHON,
     "url": URL,
     "install_requires": REQUIRED if not IS_SANDBOX else [],
-    "tests_require": tests_require,
-    "extras_require": extras,
     "packages": find_packages(),
     "include_package_data": True,
     "license": "Apache License 2.0",
