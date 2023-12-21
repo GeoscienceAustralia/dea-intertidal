bfae8b87fd1af7f1c146c3eda583bd0e2f5442ac
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-12-21 04:06:15 +0000

    Use correct path

--- tests/test_intertidal.py
+++ tests/test_intertidal.py
@@ -113,7 +113,6 @@ def test_dem_accuracy(
     )
 
     plt.savefig(f"artifacts/validation.jpg", dpi=150, bbox_inches="tight")
-    plt.savefig(f"mnt/artifacts/validation.jpg", dpi=150, bbox_inches="tight")
 
 
 def test_elevation(satellite_ds):
