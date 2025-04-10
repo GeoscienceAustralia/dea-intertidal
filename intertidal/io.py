05557b15ee4aae659900444b102ac924eb7a5b07
Claire Phillips <claire.phillips@ga.gov.au>
2025-04-10 07:09:35 +0000

    return to uint16 with correct nodata value set

--- intertidal/io.py
+++ intertidal/io.py
@@ -915,7 +915,7 @@ def prepare_for_export(
             # QA layers
             "qa_ndwi_freq": (np.uint8, 255),
             "qa_count_clear": (np.int16, -999),
-            "qa_coastal_mask": (np.int16, -999),
+            "qa_coastal_mask": (np.uint16, 65535),
         }
 
     # Apply to each array in the input `ds`
