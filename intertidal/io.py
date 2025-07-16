c4be5063a52c328ae09e703e5aeb070ad0d0ad1e
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2025-07-16 16:28:47 +1000

    Try and fix datacube 1.9 issue

--- intertidal/io.py
+++ intertidal/io.py
@@ -31,6 +31,7 @@ from eodatasets3.scripts.tostac import json_fallback
 from eodatasets3.verify import PackageChecksum
 from eodatasets3.stac import to_stac_item, validate_item
 from datacube.utils.masking import mask_invalid_data
+from datacube.utils.geometry import Geometry as Geometry_datacube18
 
 from intertidal.utils import configure_logging
 
@@ -120,7 +121,7 @@ def extract_geobox(
     """
 
     # List of valid input geometry types (from `odc-geo` or `datacube-core`)
-    GEOM_TYPES = (odc.geo.geom.Geometry, datacube.utils.geometry._base.Geometry)
+    GEOM_TYPES = (odc.geo.geom.Geometry, Geometry_datacube18)
 
     # Either `study_area` or `geom` must be provided
     if study_area is None and geom is None:
